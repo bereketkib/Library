48b77c14346aed0b4a45c9a4d0476be6959293ac
Bereket Kibreab <bekibreab@gmail.com>
2023-09-28 06:15:54 +0400

    change function constructor into class

--- main.js
+++ main.js
@@ -15,11 +15,13 @@ const cards = document.getElementById("cards");
 
 const myLibrary = [];
 
-function Book(title, author, pages, isRead) {
+class Book{
+  constructor(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+  }
 }
 
 function addBookToLibrary(book){
